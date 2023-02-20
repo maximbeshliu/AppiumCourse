56c79fed1fdc995976cc005f2611627f0cc64749
Maximbeshliu <maximbeshliu@yahoo.com>
2023-02-20 20:20:37 +0200

    eComm

---
+++ readme.txt
@@ -0,0 +1,17 @@
+System variables that has to be added
+ANDROID_HOME C:\Users\Maxim\AppData\Local\Android\Sdk
+NODE_HOME  C:\Program Files\nodejs
+
+path:
+C:\Users\Maxim\AppData\Local\Android\Sdk\tools
+C:\Users\Maxim\AppData\Local\Android\Sdk\tools\bin
+C:\Program Files\nodejs\node_modules\npm\bin
+C:\Users\Maxim\AppData\Local\Android\Sdk\platform-tools
+
+
+Appium Inspector
+
+app  path to apk
+deviceName emulatorname
+platformName  android
+automationName UiAutomator2
--- src/test/java/GeneralStore/eCommerceForm.java
+++ src/test/java/GeneralStore/eCommerceForm.java
@@ -9,11 +9,17 @@ import static FirstTests.Utilities.scrollIntoView;
 
 public class eCommerceForm extends BaseTest {
     @Test
-    public void fillTheForm() {
+    public void fillTheForm() throws InterruptedException {
         driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-        scrollIntoView(driver,"Moldova, Republic of");
+        scrollIntoView(driver, "Moldova, Republic of");
         WebElement country = driver.findElement(By.xpath("//android.widget.TextView[@text='Moldova, Republic of']"));
         country.click();
-        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Maxim Besliu");
+//        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Glasha");
+        driver.hideKeyboard();
+        driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        String popUp=driver.findElement(By.xpath("//android.widget.Toast")).getAttribute("name");
+        System.out.println(popUp);
+        Thread.sleep(5000);
     }
 }
